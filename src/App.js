d1e27378e16951756629b8e11dfdeda5335fffb1
Patricia Lambertz <lambertz.patricia@gmail.com>
2023-06-20 19:41:29 +0200

    child to parent component communication (bottum-up)

--- src/App.js
+++ src/App.js
@@ -23,9 +23,13 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = (expense) => {
+    console.log("in App.js");
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
